7684cbf5a92f2106a06098aeeade12e85cb95aad
ValentinoMartz <valentinomartinez55@gmail.com>
2023-01-12 20:33:45 -0300

    AdmMedico metodos hechos

---
+++ Negocio/AdmMedico.cs
@@ -0,0 +1,41 @@
+﻿using Entidadez;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public static class AdmMedico
+    {
+        public static List<Medico> Listar()
+        {
+            //TODO...
+            return null;
+        }
+        public static List<Medico> Listar(string especialidad)
+        {
+            //TODO...
+            return null;
+        }
+        public static int Insertar(Medico medico)
+        {
+            //TODO...
+            return 0;
+        }
+        public static int Eliminar (int id)
+        {
+            //TODO...
+            return 0;
+        }
+        public static Medico TraerUno(int id)
+        {
+            //TODO...
+            return null;
+        }
+
+    }
+}
+
+    
