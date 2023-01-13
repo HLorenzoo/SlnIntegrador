6227b63bbfc69cf7a33f91323b62f21f16eb48e8
Martina <martinaceschan@gmail.com>
2023-01-13 10:39:18 -0300

    admpaciente hecho

---
+++ Negocio/AdmPaciente.cs
@@ -0,0 +1,34 @@
+﻿using Entidadez;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public static class AdmPaciente
+    {
+        public static List<Paciente> Listar()
+        {
+            //TODO...
+            return null;
+        }
+        public static int Insertar(Paciente paciente)
+        {
+            //TODO...
+            return 0;
+        }
+        public static int Eliminar(int id)
+        {
+            //TODO...
+            return 0;
+        }
+        public static Paciente TraerUno(int NroHistoriaClinica)
+        {
+            //TODO...
+            return null;
+        }
+
+    }
+}
